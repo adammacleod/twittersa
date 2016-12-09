686f30138bd2f2d906f4f400d4e46e954e8be251
Adam MacLeod <adam@macleod.id.au>
2016-12-09 15:00:49 +1100

    Cleaned up clean-up code.

--- server.R
+++ server.R
@@ -18,44 +18,24 @@ setup_twitter_oauth(twitter_api_key,
 shinyServer(function(input, output) {
   
   sent_df <- reactive({
-    tweets <- searchTwitter(input$hashtag, n=20)
+    tweets <- searchTwitter(input$hashtag, n=50)
     tweets <- strip_retweets(tweets, strip_manual=TRUE, strip_mt=TRUE)
     
     some_txt = sapply(tweets, function(x) x$getText())
     
-    # remove retweet entities
-    # some_txt = gsub("(RT|via)((?:\\b\\W*@\\w+)+)", "", some_txt)
     # remove at people
-    #some_txt = gsub("@\\w+", "", some_txt)
+    some_txt = gsub("@\\w+", "", some_txt)
     # remove punctuation
-    #some_txt = gsub("[[:punct:]]", "", some_txt)
+    some_txt = gsub("[[:punct:]]", "", some_txt)
     # remove numbers
-    # some_txt = gsub("[[:digit:]]", "", some_txt)
+    some_txt = gsub("[[:digit:]]", "", some_txt)
     # remove html links
-    #some_txt = gsub("http\\w+", "", some_txt)
-    # remove unnecessary spaces
-    #some_txt = gsub("[ \t]{2,}", "", some_txt)
-    #some_txt = gsub("^\\s+|\\s+$", "", some_txt)
+    some_txt = gsub("http\\w+", "", some_txt)
     
-    # define "tolower error handling" function 
-    try.error = function(x)
-    {
-      # create missing value
-      y = NA
-      # tryCatch error
-      try_error = tryCatch(tolower(x), error=function(e) e)
-      # if not an error
-      if (!inherits(try_error, "error"))
-        y = tolower(x)
-      # result
-      return(y)
-    }
-    # lower case using try.error with sapply 
-    some_txt = sapply(some_txt, try.error)
-    
-    # remove NAs in some_txt
-    #some_txt = some_txt[!is.na(some_txt)]
-    #names(some_txt) = NULL
+    # Strangely enough, it seems that tolower can't handle unicode.
+    some_txt = sapply(some_txt, function(txt) {
+      tryCatch(tolower(txt), error = function(e) { return("") })
+    })
     
     # classify emotion
     class_emo = classify_emotion(some_txt, algorithm="bayes", prior=1.0)
